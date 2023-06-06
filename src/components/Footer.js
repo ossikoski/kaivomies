123653512472eab1912494a53037913034b08676
ossikoski <ossioskari@gmail.com>
2023-06-06 11:09:48 +0300

    Lisätty tekstit, muokattu footer ja lisätty sivun title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import igLogo from '../img/ig_logo.png'
 import fbLogo from '../img/fb_logo.png'
+import logoSquare from '../img/logo230603_square.png'
 
-const Footer = () => {
+const FooterOld = () => {
 
     return(
         <div className='footer'><div className='footerText'>
-            <h2 style={{marginTop: '30px'}}>Ota yhteyttä</h2>
+            <h3>Ota yhteyttä</h3>
+            Tmi Kaivomies, Y-tunnus: 3365293-7<br></br>
             Elias Mako<br></br>
             puh. 0400781589<br></br>
-            kaivomiesoy(at)gmail.com<br></br><br></br>
+            tmikaivomies(at)gmail.com<br></br><br></br>
             <a href='https://www.instagram.com' target='_blank' rel="noreferrer" style={{ marginRight: '10px' }}>
                 <img className='socialsPic' src={ igLogo } alt="Instagram" />
             </a>
@@ -20,4 +22,48 @@ const Footer = () => {
     )
 }
 
+
+
+
+const Footer = () => {
+
+    return(
+        <div class="foot">
+            <div class="left">
+                Tmi Kaivomies<br></br>
+                Y-tunnus: 3365293-7
+            </div>
+
+            <div class="middle">
+                <h3>Ota yhteyttä</h3>
+                Elias Mako, puh. 0400781589<br></br>
+                tmikaivomies(at)gmail.com
+            </div>
+
+            <div class="right">
+                <a href='https://www.instagram.com' target='_blank' rel="noreferrer" style={{ marginRight: '10px' }}>
+                    <img className='socialsPic' src={ igLogo } alt="Instagram" />
+                </a>
+                <a href='https://www.facebook.com' target='_blank' rel="noreferrer">
+                    <img className='socialsPic' src={ fbLogo } alt="Facebook" />
+                </a>
+            </div>
+        </div>
+
+    )
+}
+
 export default Footer
+{/* <div className='footer'><div className='footerText'>
+<h3>Ota yhteyttä</h3>
+Tmi Kaivomies, Y-tunnus: 3365293-7<br></br>
+Elias Mako<br></br>
+puh. 0400781589<br></br>
+tmikaivomies(at)gmail.com<br></br><br></br>
+<a href='https://www.instagram.com' target='_blank' rel="noreferrer" style={{ marginRight: '10px' }}>
+    <img className='socialsPic' src={ igLogo } alt="Instagram" />
+</a>
+<a href='https://www.facebook.com' target='_blank' rel="noreferrer">
+    <img className='socialsPic' src={ fbLogo } alt="Facebook" />
+</a>
+</div></div> */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,25 +7,40 @@ const Home = () => {
             <h2>
                 Miksi puhdistaa kaivo?
             </h2>
-            Jotain
+            <br></br>
+            Vesirengaskaivot keräävät ajan myötä pohjalle ja renkaiden sisäpinnalle lietettä.
+            Tämä aiheuttaa veden kirkkauden heikentymistä ja mahdollista makuhaittaa.
+            Ylärenkaiden väleistä saattaa päästä pintavettä kaivoon, joten ne tulisi saumata umpeen.
+            Huonojen kansirakenteiden takia kaivoon voi päästä eläimiä,
+            kuten pieniä nisäkkäitä, sammakoita tai etanoita. 
+            <br></br><br></br>
+            Kaivojen huoltotarve vaihtelee:
+            esimerkiksi jotkin rauta- tai mangaanipitoista vettä tuottavat kaivot
+            keräävät veteen väriä niin sankoin määrin, että niiden puhdistus täytyy tehdä
+            tyypillistä 5-10 vuoden huoltoväliä useammin.
+            Käyttämättömissä kaivoissa seisova vesi alkaa monesti haista,
+            joten runsas käyttö vedentuoton puitteissa on kaivolle vain hyväksi.
 
             <h2>
                 Mitä kaivonhuoltotyöhön kuuluu?
-            </h2>
-            <ul>
-                <li>asd</li>
-                <li>asdg</li>
-                <li>adfh</li>
-            </ul>
+            </h2><br></br>
+            Kaivohuoltotyöhön kuuluu kaivon ulkoinen ja sisäinen tarkistus,
+            veden PH-arvon mittaus, kaivonrenkaiden pesu, pohjalietteiden poisto,
+            yläsaumojen tiivistäminen ja veden desinfiointi.
+            Pohjan koostumuksen ja veden PH-arvon mukaan kaivon pohjalle lisätään tarvittaessa
+            kalkkia ja suodatinhiekkaa.
+            <br></br><br></br>
             <h3>
                 Hinnoittelu
             </h3>
-            Maksaa
-
-            <h2>
-                Kotitalousvähennys
-            </h2>
-            Muista, että kaivonhuollosta voit saada 50% verovähennystä!
+            <ul>
+                <li>100cm ja 80cm sisähalkaisija 495€</li>
+                <li>120cm sisähalkaisija +100€</li>
+                <li>150cm sisähalkaisija +150€</li>
+                <li>yli 7m syvä kaivo +100€</li>
+            </ul>
+            Muista, että kaivohuollon voit merkitä veroilmoituksen kotitalousvähennyksiin ja
+            saada 50% verovähennystä!
         </div>
     )
 }
