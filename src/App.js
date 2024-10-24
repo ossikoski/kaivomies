66e5e75e86900c8f3d8b22f55bd89a8bd840ef36
Ossi Koski <ossi.koski@wapice.com>
2024-10-24 21:49:10 +0300

    index.html schema.org lisäys (kaikki toimialueet), public logojen vaihto, sitemap päiväyksen päivitys, notifikaatiokomponentin lisäys (kausi 2024 paketissa) ja fonttikokojen twiikkaus.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import React from 'react'
 
 import Header from './components/Header.js'
 import Home from './components/Home.js'
-import Footer from './components/Footer';
+import Footer from './components/Footer.js'
+import Notification from './components/Notification.js'
 
 
 function App() {
   return (
     <div className='mainDiv'>
       <Header/>
+      <Notification/>
       <Home/>
       <Footer/>
     </div>
---
+++ src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return(
+        <div className='notification'>
+            <div className='notificationText'>
+                Kaivokausi 2024 on paketissa, suurkiitokset kaikille asiakkaille! Kaivohuoltoja
+                saatavilla taas keväällä 2025.
+            </div>
+        </div>
+    )
+}
+
+export default Footer
